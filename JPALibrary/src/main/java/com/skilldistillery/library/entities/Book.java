09971c84af5f220cef5433aef9291ca4f2477391
skaddington <skaddington92@gmail.com>
2023-05-12 16:35:46 -0600

    Create Book entity, test Hibernate config

---
+++ JPALibrary/src/main/java/com/skilldistillery/library/entities/Book.java
@@ -0,0 +1,64 @@
+package com.skilldistillery.library.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Book {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	private String title;
+	@Column(name = "author_first_name")
+	private String authorFirstName;
+	@Column(name = "author_last_name")
+	private String authorLastName;
+
+	public Book() {
+		super();
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthorFirstName() {
+		return authorFirstName;
+	}
+
+	public void setAuthorFirstName(String authorFirstName) {
+		this.authorFirstName = authorFirstName;
+	}
+
+	public String getAuthorLastName() {
+		return authorLastName;
+	}
+
+	public void setAuthorLastName(String authorLastName) {
+		this.authorLastName = authorLastName;
+	}
+
+	@Override
+	public String toString() {
+		return "Book [id=" + id + ", title=" + title + ", authorFirstName=" + authorFirstName + ", authorLastName="
+				+ authorLastName + "]";
+	}
+
+}
